3b43c1dcb92a0142bb7a62e4268b2b86bffd16a0
dyfan2004 <153422145+dyfan2004@users.noreply.github.com>
2023-12-10 15:40:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,212 @@
+<?php
+$host       = "localhost";
+$user       = "root";
+$pass       = "";
+$db         = "data";
+
+$koneksi    = mysqli_connect($host, $user, $pass, $db);
+if (!$koneksi) { //cek koneksi
+    die("Tidak bisa terkoneksi ke database");
+}
+$npm        = "";
+$nama       = "";
+$alamat     = "";
+$email   = "";
+$sukses     = "";
+$error      = "";
+
+if (isset($_GET['op'])) {
+    $op = $_GET['op'];
+} else {
+    $op = "";
+}
+if($op == 'delete'){
+    $id         = $_GET['id'];
+    $sql1       = "delete from datamahasiswa where id = '$id'";
+    $q1         = mysqli_query($koneksi,$sql1);
+    if($q1){
+        $sukses = "Berhasil hapus data";
+    }else{
+        $error  = "Gagal melakukan delete data";
+    }
+}
+if ($op == 'edit') {
+    $id         = $_GET['id'];
+    $sql1       = "select * from datamahasiswa where id = '$id'";
+    $q1         = mysqli_query($koneksi, $sql1);
+    $r1         = mysqli_fetch_array($q1);
+    $npm        = $r1['npm'];
+    $nama       = $r1['nama'];
+    $alamat     = $r1['alamat'];
+    $email   = $r1['email'];
+
+    if ($npm == '') {
+        $error = "Data tidak ditemukan";
+    }
+}
+if (isset($_POST['simpan'])) { //untuk create
+    $npm        = $_POST['npm'];
+    $nama       = $_POST['nama'];
+    $alamat     = $_POST['alamat'];
+    $email   = $_POST['email'];
+
+    if ($npm && $nama && $alamat && $email) {
+        if ($op == 'edit') { //untuk update
+            $sql1       = "update mahasiswa set npm = '$npm',nama='$nama',alamat = '$alamat',email='$email' where id = '$id'";
+            $q1         = mysqli_query($koneksi, $sql1);
+            if ($q1) {
+                $sukses = "Data berhasil diupdate";
+            } else {
+                $error  = "Data gagal diupdate";
+            }
+        } else { //untuk insert
+            $sql1   = "insert into mahasiswa(npm,nama,alamat,email) values ('$npm','$nama','$alamat','$email')";
+            $q1     = mysqli_query($koneksi, $sql1);
+            if ($q1) {
+                $sukses     = "Berhasil memasukkan data baru";
+            } else {
+                $error      = "Gagal memasukkan data";
+            }
+        }
+    } else {
+        $error = "Silakan masukkan semua data";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dyfan al-fath_50421412</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <style>
+        .mx-auto {
+            width: 800px
+        }
+
+        .card {
+            margin-top: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="mx-auto">
+        <!-- untuk memasukkan data -->
+        <div class="card">
+            <div class="card-header">
+                Create / Edit Data
+            </div>
+            <div class="card-body">
+                <?php
+                if ($error) {
+                ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo $error ?>
+                    </div>
+                <?php
+                    header("refresh:5;url=index.php");//5 : detik
+                }
+                ?>
+                <?php
+                if ($sukses) {
+                ?>
+                    <div class="alert alert-success" role="alert">
+                        <?php echo $sukses ?>
+                    </div>
+                <?php
+                    header("refresh:5;url=index.php");
+                }
+                ?>
+                <form action="" method="POST">
+                    <div class="mb-3 row">
+                        <label for="npm" class="col-sm-2 col-form-label">NPM</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="npm" name="npm" value="<?php echo $npm ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="nama" name="nama" value="<?php echo $nama ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="alamat" name="alamat" value="<?php echo $alamat ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="email" class="col-sm-2 col-form-label">email</label>
+                        <div class="col-sm-10">
+                            <select class="form-control" name="email" id="email">
+                                <option value="">- Pilih Fakultas -</option>
+                                <option value="Manajemen" <?php if ($email == "Manajemen") echo "selected" ?>>Manajemen</option>
+                                <option value="Teknik Informatika" <?php if ($email == "Teknik Informatika") echo "selected" ?>>Teknik Informatika</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <input type="submit" name="simpan" value="Simpan Data" class="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <!-- untuk mengeluarkan data -->
+        <div class="card">
+            <div class="card-header text-white bg-secondary">
+                Data Mahasiswa
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">NPM</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Alamat</th>
+                            <th scope="col">email</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql2   = "select * from datamahasiswa order by id desc";
+                        $q2     = mysqli_query($koneksi, $sql2);
+                        $urut   = 1;
+                        while ($r2 = mysqli_fetch_array($q2)) {
+                            $id         = $r2['id'];
+                            $npm        = $r2['npm'];
+                            $nama       = $r2['nama'];
+                            $alamat     = $r2['alamat'];
+                            $email   = $r2['email'];
+
+                        ?>
+                            <tr>
+                                <th scope="row"><?php echo $urut++ ?></th>
+                                <td scope="row"><?php echo $npm ?></td>
+                                <td scope="row"><?php echo $nama ?></td>
+                                <td scope="row"><?php echo $alamat ?></td>
+                                <td scope="row"><?php echo $email ?></td>
+                                <td scope="row">
+                                    <a href="index.php?op=edit&id=<?php echo $id ?>"><button type="button" class="btn btn-warning">Edit</button></a>
+                                    <a href="index.php?op=delete&id=<?php echo $id?>" onclick="return confirm('Yakin mau delete data?')"><button type="button" class="btn btn-danger">Delete</button></a>            
+                                </td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                    
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
